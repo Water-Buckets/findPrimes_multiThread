76220aec0a0929f65efb9561a293c26f9f1ba425
WaterBucket <lizirui3724@gmail.com>
2023-07-11 13:40:06 +0800

    Add execution time tracking to multi-threading module
    Added execution time tracking for different stages in the multi-threading module including pre-sieveing, sieving and writing to file. This will provide insights into the time taken by different stages, helping in identifying bottlenecks and areas of optimizations in the multi-threading functions.

--- src/multiThread.cpp
+++ src/multiThread.cpp
@@ -7,11 +7,17 @@
 
 void multiThread(const int &threads, const char &m, const long long &n, const std::string &file) {
     if (m == 'e') exit(0);
+    auto startPre = std::chrono::steady_clock::now();
     long long sqrtN = std::sqrt(n);
     std::vector<long long> preSievedPrimes;
     preSieve(sqrtN, m, preSievedPrimes);
     std::vector<long long> primes;
     primes.insert(primes.end(), preSievedPrimes.begin(), preSievedPrimes.end());
+    auto endPre = std::chrono::steady_clock::now();
+    auto diffPre = std::chrono::duration_cast<std::chrono::nanoseconds>(endPre - startPre);
+    long long durationPre = diffPre.count();
+    std::cout << "Time elapsed pre-sieveing:" << double(durationPre) / 1000000000 << " seconds." << std::endl;
+    auto start = std::chrono::steady_clock::now();
     std::vector<std::thread> vthread;
     long long perThread = (n - sqrtN) / threads;
     std::vector<std::vector<long long>> primesVec(threads);
@@ -27,6 +33,11 @@ void multiThread(const int &threads, const char &m, const long long &n, const st
     for (auto &thr: vthread) {
         if (thr.joinable()) thr.join();
     }
+    auto end = std::chrono::steady_clock::now();
+    auto diff = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+    long long duration = diff.count();
+    std::cout << "Time elapsed sieveing:" << double(duration) / 1000000000 << " seconds." << std::endl;
+    auto startWrite = std::chrono::steady_clock::now();
     std::ofstream outfile(file);
     if (!outfile.is_open()) throw std::runtime_error("Failed to open file.");
     for (long long i: preSievedPrimes) {
@@ -38,4 +49,8 @@ void multiThread(const int &threads, const char &m, const long long &n, const st
         }
     }
     outfile.close();
+    auto endWrite = std::chrono::steady_clock::now();
+    auto diffWrite = std::chrono::duration_cast<std::chrono::nanoseconds>(endWrite - startWrite);
+    long long durationWrite = diffWrite.count();
+    std::clog << "Time elapsed writing to file: " << double(durationWrite) / 1000000000 << " seconds." << std::endl;
 }
