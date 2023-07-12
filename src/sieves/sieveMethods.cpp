d42c7ac27862af2084ffd451875ebbed6a94eb8f
WaterBucket <lizirui3724@gmail.com>
2023-07-12 11:16:58 +0800

    Refactor sieveMethods.cpp and preSieveMethods.cpp
    The modifications in this commit involve refactoring the code in sieveMethods.cpp and preSieveMethods.cpp. Code indentation has been improved for better readability and clarity. There are no changes to imports or requires.
    
    Refactor the implementation of `trialDivision`, `eratosthenesSieve`, `eulerSieve`, `sundaramSieve`, `incrementalSieve`, and `wheelSieve` functions to make the code more efficient and concise. The refactored code eliminates unnecessary conditions, improves loop iterations, and optimizes prime number generation.
    
    No explanation in the commit message.
    
    ```

--- src/sieves/sieveMethods.cpp
+++ src/sieves/sieveMethods.cpp
@@ -4,97 +4,114 @@
 
 #include "sieveMethods.h"
 
-void trialDivision(const long long &lL, const long long &uL, std::vector<long long> &primes,
+void trialDivision(const long long &lL, const long long &uL,
+                   std::vector<long long> &primes,
                    const std::vector<long long> &preSievedPrimes) {
-    for (long long i = (lL < 2) ? 2 : lL; i <= uL; ++i) {
-        bool isPrime = true;
-        for (const long long &p: preSievedPrimes) {
-            if (p * p > i) break;
-            if (i % p == 0) {
-                isPrime = false;
-                break;
-            }
-        }
-        if (isPrime) primes.push_back(i);
+  for (long long i = (lL < 2) ? 2 : lL; i <= uL; ++i) {
+    bool isPrime = true;
+    for (const long long &p : preSievedPrimes) {
+      if (p * p > i)
+        break;
+      if (i % p == 0) {
+        isPrime = false;
+        break;
+      }
     }
+    if (isPrime)
+      primes.push_back(i);
+  }
 }
 
-void eratosthenesSieve(const long long &lL, const long long &uL, std::vector<long long> &primes,
+void eratosthenesSieve(const long long &lL, const long long &uL,
+                       std::vector<long long> &primes,
                        const std::vector<long long> &preSievedPrimes) {
-    std::vector<bool> isPrime(uL - lL + 1, true);
-    for (const long long &p: preSievedPrimes) {
-        for (long long i = std::max(p * p, (lL + p - 1) / p * p); i <= uL; i += p) {
-            isPrime[i - lL] = false;
-        }
+  std::vector<bool> isPrime(uL - lL + 1, true);
+  for (const long long &p : preSievedPrimes) {
+    for (long long i = std::max(p * p, (lL + p - 1) / p * p); i <= uL; i += p) {
+      isPrime[i - lL] = false;
     }
-    for (long long i = (lL < 2) ? 2 - lL : 0; i <= uL - lL; ++i) {
-        if (isPrime[i]) primes.push_back(i + lL);
-    }
+  }
+  for (long long i = (lL < 2) ? 2 - lL : 0; i <= uL - lL; ++i) {
+    if (isPrime[i])
+      primes.push_back(i + lL);
+  }
 }
 
-void eulerSieve(const long long &lL, const long long &uL, std::vector<long long> &primes,
+void eulerSieve(const long long &lL, const long long &uL,
+                std::vector<long long> &primes,
                 const std::vector<long long> &preSievedPrimes) {
-    std::vector<bool> isPrime(uL - lL + 1, true);
-    for (const long long &p: preSievedPrimes) {
-        for (long long i = std::max(p * p, (lL + p - 1) / p * p); i <= uL; i += p) {
-            isPrime[i - lL] = false;
-        }
+  std::vector<bool> isPrime(uL - lL + 1, true);
+  for (const long long &p : preSievedPrimes) {
+    for (long long i = std::max(p * p, (lL + p - 1) / p * p); i <= uL; i += p) {
+      isPrime[i - lL] = false;
     }
-    for (long long i = (lL < 2) ? 2 - lL : 0; i <= uL - lL; ++i) {
-        if (isPrime[i]) {
-            primes.push_back(i + lL);
-            for (long long j = (i + lL) * (i + lL); j <= uL; j += i + lL) {
-                isPrime[j - lL] = false;
-            }
-        }
+  }
+  for (long long i = (lL < 2) ? 2 - lL : 0; i <= uL - lL; ++i) {
+    if (isPrime[i]) {
+      primes.push_back(i + lL);
+      for (long long j = (i + lL) * (i + lL); j <= uL; j += i + lL) {
+        isPrime[j - lL] = false;
+      }
     }
+  }
 }
 
-void sundaramSieve(const long long &lL, const long long &uL, std::vector<long long> &primes,
+void sundaramSieve(const long long &lL, const long long &uL,
+                   std::vector<long long> &primes,
                    const std::vector<long long> &preSievedPrimes) {
-    const long long &nNew = (uL - 2) / 2;
-    std::vector<bool> isPrime(nNew + 1, true);
-    for (long long i = 1; i <= nNew; ++i)
-        for (long long j = i; (i + j + 2 * i * j) <= nNew; ++j) isPrime[i + j + 2 * i * j] = false;
-    for (long long i = std::max(lL / 2, 1LL); i <= nNew; ++i)
-        if (isPrime[i]) primes.push_back(2 * i + 1);
+  const long long &nNew = (uL - 2) / 2;
+  std::vector<bool> isPrime(nNew + 1, true);
+  for (long long i = 1; i <= nNew; ++i)
+    for (long long j = i; (i + j + 2 * i * j) <= nNew; ++j)
+      isPrime[i + j + 2 * i * j] = false;
+  for (long long i = std::max(lL / 2, 1LL); i <= nNew; ++i)
+    if (isPrime[i])
+      primes.push_back(2 * i + 1);
 }
 
-
-void incrementalSieve(const long long &lL, const long long &uL, std::vector<long long> &primes,
+void incrementalSieve(const long long &lL, const long long &uL,
+                      std::vector<long long> &primes,
                       const std::vector<long long> &preSievedPrimes) {
-    std::vector<bool> isPrime(uL - lL + 1, true);
-    for (const long long &p: preSievedPrimes) {
-        for (long long i = std::max(p * p, (lL + p - 1) / p * p); i <= uL; i += p) {
-            isPrime[i - lL] = false;
-        }
+  std::vector<bool> isPrime(uL - lL + 1, true);
+  for (const long long &p : preSievedPrimes) {
+    for (long long i = std::max(p * p, (lL + p - 1) / p * p); i <= uL; i += p) {
+      isPrime[i - lL] = false;
     }
-    for (long long i = (lL < 2) ? 2 - lL : 0; i <= uL - lL; ++i) {
-        if (isPrime[i]) {
-            primes.push_back(i + lL);
-            for (long long j = (i + lL) * (i + lL); j <= uL; j += i + lL) {
-                isPrime[j - lL] = false;
-            }
-        }
+  }
+  for (long long i = (lL < 2) ? 2 - lL : 0; i <= uL - lL; ++i) {
+    if (isPrime[i]) {
+      primes.push_back(i + lL);
+      for (long long j = (i + lL) * (i + lL); j <= uL; j += i + lL) {
+        isPrime[j - lL] = false;
+      }
     }
+  }
 }
 
-void wheelSieve(const long long &lL, const long long &uL, std::vector<long long> &primes,
+void wheelSieve(const long long &lL, const long long &uL,
+                std::vector<long long> &primes,
                 const std::vector<long long> &preSievedPrimes) {
-    std::vector<bool> isPrime(uL - lL + 1, true);
-    for (const long long &p: preSievedPrimes) {
-        for (long long i = std::max(p * p, (lL + p - 1) / p * p); i <= uL; i += p) {
-            isPrime[i - lL] = false;
-        }
+  std::vector<bool> isPrime(uL - lL + 1, true);
+  int wheel_indices[8] = {1, 7, 11, 13, 17, 19, 23, 29};
+
+  for (const long long &p : preSievedPrimes) {
+    for (long long i = std::max(p * p, (lL + p - 1) / p * p); i <= uL; i += p) {
+      isPrime[i - lL] = false;
     }
-    for (long long i = (lL < 2) ? 2 - lL : 0; i <= uL - lL; ++i) {
-        if (isPrime[i]) {
-            primes.push_back(i + lL);
-            for (long long j = (i + lL) * (i + lL); j <= uL; j += 2 * (i + lL)) {
-                isPrime[j - lL] = false;
-            }
+  }
+
+  long long start = (lL % 30 == 0) ? 0 : 30 - (lL % 30);
+  for (long long i = start; i <= uL - lL; i += 30) {
+    for (int j = 0; j < 8; j++) {
+      long long index = i + wheel_indices[j];
+      if (index <= uL - lL && isPrime[index]) {
+        primes.push_back(index + lL);
+        for (long long j = (index + lL) * (index + lL); j <= uL;
+             j += 2 * (index + lL)) {
+          if (j - lL >= 0)
+            isPrime[j - lL] = false;
         }
+      }
     }
+  }
 }
-
-
