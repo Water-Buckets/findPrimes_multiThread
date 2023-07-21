490e9adfdb6e217b15f834184be8c50e1b5e1204
WaterBucket <lizirui3724@gmail.com>
2023-07-21 08:04:45 +0800

    Change argument checking logic in argInput function
    The argument checking logic in the `argInput` function has been modified to include try-catch blocks. Previously, the try-catch blocks were only present for certain parts of the code. Now, the entire function is wrapped in a try block and appropriate catch blocks are added to handle exceptions. This change ensures that any invalid arguments passed to the function will be caught and handled correctly.

--- src/argInput.cpp
+++ src/argInput.cpp
@@ -4,42 +4,41 @@
 #include "includes/argInput.h"
 
 void argInput(char *argv[]) {
-
+  try {
 #ifndef NDEBUG
-  std::cerr << __func__ << std::endl;
+    std::cerr << __func__ << std::endl;
 #endif
 
-  // 检查输入是否合法
-  if (!isNumeric(argv[1]) || !isNumeric(argv[3]) || argv[2][1])
-    throw std::invalid_argument("Invalid input.");
+    // 检查输入是否合法
+    if (!isNumeric(argv[1]) || !isNumeric(argv[3]) || argv[2][1])
+      throw std::invalid_argument("Invalid input.");
 
-  // 将输入转换为合适的类型
-  int threads = std::stoi(argv[1]);
-  char m = argv[2][0];
-  long long n = std::stoll(argv[3]);
-  std::string file = argv[4];
+    // 将输入转换为合适的类型
+    int threads = std::stoi(argv[1]);
+    char m = argv[2][0];
+    long long n = std::stoll(argv[3]);
+    std::string file = argv[4];
 
-  // 检查输入是否合法
-  if (n <= 1)
-    throw std::invalid_argument("n must be greater than 1.");
-
-#ifndef NDEBUG
-  std::cerr << __func__ << ": " << threads << " " << m << " " << n << " "
-            << file << std::endl;
-#endif
-
-  if (threads == 1) {
-    std::cout << "Using monoThread mode" << std::endl;
-    void (*pMonoMethod)(const long long &n, std::vector<long long> &primes);
-    pMonoMethod = switchMonoMethods(m);
+    // 检查输入是否合法
+    if (n <= 1)
+      throw std::invalid_argument("n must be greater than 1.");
 
 #ifndef NDEBUG
-    std::cerr << "pMonoMethod: " << pMonoMethod << std::endl;
+    std::cerr << __func__ << ": " << threads << " " << m << " " << n << " "
+              << file << std::endl;
 #endif
 
-    monoThread(n, file, pMonoMethod);
-  } else if (threads > 1) {
-    try {
+    if (threads == 1) {
+      std::cout << "Using monoThread mode" << std::endl;
+      void (*pMonoMethod)(const long long &n, std::vector<long long> &primes);
+      pMonoMethod = switchMonoMethods(m);
+
+#ifndef NDEBUG
+      std::cerr << "pMonoMethod: " << pMonoMethod << std::endl;
+#endif
+
+      monoThread(n, file, pMonoMethod);
+    } else if (threads > 1) {
       std::cout << "Using multiThread mode" << std::endl;
       if (m == 'e' || m == 'g')
         throw std::invalid_argument(
@@ -62,17 +61,17 @@ void argInput(char *argv[]) {
 #endif
 
       multiThread(threads, n, file, pMultiMethod, pPreSieveMethod);
-    } catch (std::invalid_argument &e) {
-      std::cerr << e.what() << std::endl;
-      std::exit(0);
-    } catch (std::runtime_error &e) {
-      std::cerr << e.what() << std::endl;
-      std::cerr
-          << "Please submit you issues here: "
-             "https://github.com/Water-Buckets/findPrimes_multiThread/issues"
-          << std::endl;
-      std::exit(0);
-    }
-  } else
-    throw std::invalid_argument("Invalid number of threads");
+    } else
+      throw std::invalid_argument("Invalid number of threads");
+  } catch (std::invalid_argument &e) {
+    std::cerr << e.what() << std::endl;
+    std::exit(0);
+  } catch (std::runtime_error &e) {
+    std::cerr << e.what() << std::endl;
+    std::cerr
+        << "Please submit you issues here: "
+           "https://github.com/Water-Buckets/findPrimes_multiThread/issues"
+        << std::endl;
+    std::exit(0);
+  }
 }
